8116cab3b1812f31c034b214570b156153d9d4e6
Luciene Santos <lucienesantosmc@gmail.com>
2019-09-13 14:34:37 -0300

    Add context to toogle sidebar to show link name

--- src/components/globalsidebar/nav/NavText.js
+++ src/components/globalsidebar/nav/NavText.js
@@ -1,18 +1,24 @@
 import React from "react";
 import {styled} from "../../../ui-kit/styled";
+import {GlobalSideBarContextConsumer} from "../../../contexts/GlobalSideBarContext";
 
 const NavText = props => {
   return (
-    <styled.div
-      css={{
-        marginLeft: "5px",
-        color: "white",
-        marginTop: "2px",
-        marginLeft: "16px",
-        fontWeight: "400"
-      }}>
-      {props.name}
-    </styled.div>
+    <GlobalSideBarContextConsumer>
+      {value => (
+        <styled.div
+          css={{
+            marginLeft: "5px",
+            color: "white",
+            marginTop: "2px",
+            marginLeft: "16px",
+            fontWeight: "400",
+            display: value.expandedGlobalSideBar ? "block" : "none"
+          }}>
+          {props.name}
+        </styled.div>
+      )}
+    </GlobalSideBarContextConsumer>
   );
 };
 
